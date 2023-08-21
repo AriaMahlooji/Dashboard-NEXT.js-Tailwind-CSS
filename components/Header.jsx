abf0c4a8d9cd44454c8400dc1f3f23f143ec8dc1
AriaMahlouji <ariamahlooji.de@gmail.com>
2023-08-21 18:54:27 +0200

    adding side bar and top cards

---
+++ components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='flex justify-between px-4 pt-4'>
+      <div>Dashboard</div>
+      <div>welcome back client</div>
+    </div>
+  )
+}
+
+export default Header
